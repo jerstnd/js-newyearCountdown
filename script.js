a22b4091a805f735dc19345221b37ab2485157f9
Jeremy Rustandi <jeremyandika21@gmail.com>
2021-05-27 16:37:30 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const countdown = () => {
+    const countDate = new Date("Jan 1, 2022 00:00:00").getTime();
+    const today = new Date().getTime();
+    const gap = countDate - today;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap/day);
+    const textHour = Math.floor((gap % day)/ hour);
+    const textMinute = Math.floor((gap % hour)/ minute);
+    const textSecond= Math.floor((gap % minute)/ second);
+
+    document.querySelector(".day").innerHTML = textDay;
+    document.querySelector(".hour").innerHTML = textHour;
+    document.querySelector(".minute").innerHTML = textMinute;
+    document.querySelector(".second").innerHTML = textSecond;
+ 
+}
+
+setInterval(countdown, 1000);
